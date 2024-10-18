aaa52ba2358c64541da3b8958be5891983098e83
Phill <philldamaskin@gmail.com>
2024-10-18 19:11:36 -0400

    generic singleton type

---
+++ source/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Simulation
+{
+    public readonly struct Singleton<T> where T : IDisposable, new()
+    {
+        private static readonly T instance;
+        private static int references;
+
+        static Singleton()
+        {
+            instance = new();
+        }
+
+        public static T Retrieve()
+        {
+            references++;
+            return instance;
+        }
+
+        public static void Return()
+        {
+            references--;
+            if (references < 0)
+            {
+                throw new InvalidOperationException("Singleton was returned more times than it was retrieved");
+            }
+
+            if (references == 0)
+            {
+                instance.Dispose();
+            }
+        }
+    }
+}
