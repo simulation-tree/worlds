b127d8f0cbac51a7de003399686079d4b42ea091
Phill <philldamaskin@gmail.com>
2025-02-03 14:51:04 -0500

    return explicit default when entity isnt found

--- core/Extensions/EnumerableExtensions.cs
+++ core/Extensions/EnumerableExtensions.cs
@@ -82,7 +82,15 @@ namespace Worlds
             }
 
             bool found = query.TryGetFirst(out uint foundEntity);
-            entity = new Entity(world, foundEntity).As<T>();
+            if (found)
+            {
+                entity = new Entity(world, foundEntity).As<T>();
+            }
+            else
+            {
+                entity = default;
+            }
+
             return found;
         }
 
