55018ecc08e67f36e3f2e281a9d870a80c100581
Phill <philldamaskin@gmail.com>
2025-04-17 20:21:53 -0400

    unity compat

--- core/Arrays.cs
+++ core/Arrays.cs
@@ -12,10 +12,23 @@ namespace Worlds
             set => buffer[index] = value;
         }
 
+#if NET
         [InlineArray(BitMask.Capacity)]
         private struct Buffer
         {
             private Values element0;
         }
+#else
+        private unsafe struct Buffer
+        {
+            private fixed ulong buffer[BitMask.Capacity];
+
+            public Values this[int index]
+            {
+                readonly get => new((nint)buffer[index]);
+                set => buffer[index] = (ulong)(nint)value.pointer;
+            }
+        }
+#endif
     }
 }
