ebf196b8a06cf5848faefe8e12f9aadb4365674a
Phill <philldamaskin@gmail.com>
2025-06-01 12:20:35 -0400

    add update message

---
+++ core/Messages/Update.cs
@@ -0,0 +1,26 @@
+﻿namespace Worlds.Messages
+{
+    /// <summary>
+    /// Message type for a world update.
+    /// </summary>
+    public readonly struct Update
+    {
+        /// <summary>
+        /// The delta time for the update.
+        /// </summary>
+        public readonly double deltaTime;
+
+        /// <summary>
+        /// The delta time for the update as a float.
+        /// </summary>
+        public readonly float DeltaTimeAsFloat => (float)deltaTime;
+
+        /// <summary>
+        /// Initializes the message.
+        /// </summary>
+        public Update(double deltaTime)
+        {
+            this.deltaTime = deltaTime;
+        }
+    }
+}
